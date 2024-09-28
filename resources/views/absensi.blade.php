8eb86827d50a82d0aed7e709bd347da1bdade664
Muhamad Anas Mustopa <muhamadanasmustopa1112@gmail.com>
2024-09-28 10:28:39 +0700

    feat : bug fixing

--- resources/views/absensi.blade.php
+++ resources/views/absensi.blade.php
@@ -236,6 +236,9 @@
                             }),
                             contentType: 'application/json',
                             success: function (response) {
+                                alert("RawId (Base64 Encoded):", rawIdBase64);
+                                alert("ClientDataJSON (Base64 Encoded):", clientDataJSONBase64);
+                                alert("AttestationObject (Base64 Encoded):", attestationObjectBase64);
                                 alert('Data dari server:\n' + JSON.stringify(response, null, 2)); // \n digunakan untuk memulai baris baru
 
                                 // if (response.status === 'success') {
