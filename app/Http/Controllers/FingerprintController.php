ec6f3a1ee8c42e50b9d2a53c3c17a3f965ae62b0
Muhamad Anas Mustopa <muhamadanasmustopa1112@gmail.com>
2024-09-28 10:21:40 +0700

    debuging 6

--- app/Http/Controllers/FingerprintController.php
+++ app/Http/Controllers/FingerprintController.php
@@ -67,7 +67,7 @@ class FingerprintController extends Controller
             return response()->json([
                 'status' => 'debug',
                 'data' => [
-                    'stored_rawId' => json_encode($storedFingerprint->raw_id),
+                    'stored_rawId' => json_decode($storedFingerprint->raw_id),
      
                 ]
             ]);
